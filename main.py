8741eedb6fca7cddda8419f2917b883490f3b022
t0nik <59863144+t0nik@users.noreply.github.com>
2022-02-16 01:07:01 +0100

    Change XML parsing framework
    miniDOM -> ElementTree

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-from xml.dom import minidom
+import xml.etree.ElementTree as ET
 
 def main():
-    # parse an xml file by name
-    file = minidom.parse("annotations\\road0.xml")
 
-    width = file.getElementsByTagName("width")
+    # parse and receive the data as a tree, get root of the tree
+    tree = ET.parse('test\\annotations\\road0.xml')
+    root = tree.getroot()
 
-    # print data
-    print('\nWidth:')
-    for children in width:
-        print(children.firstChild.data)
-
+    for bnd in root.iter('bndbox'):
+        for dir in bnd:
+            print(dir.tag + ": " + dir.text)
 
 if __name__ == '__main__':
     main()
