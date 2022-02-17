b27c8ae8eaba8ed6b5080dc658464e549266e9b7
t0nik <59863144+t0nik@users.noreply.github.com>
2022-02-17 06:54:32 +0100

    Add parse_data()

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+import os
 import xml.etree.ElementTree as ET
+import pandas
+import cv2
+import glob
+
+
+def parse_data(path):
+    data = []
+    # loop through all .xml files in path
+    for filename in glob.glob(path + '*.xml'):
+        # parse and receive the data as a tree, get root of the tree
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        # for bnd in root.iter('bndbox'):
+        #     for dir in bnd:
+        #         print(dir.tag + ": " + dir.text)
+
+        for name in root.iter('filename'):
+            data.append([name.text])
+
+    return data
+
 
 def main():
 
-    # parse and receive the data as a tree, get root of the tree
-    tree = ET.parse('test\\annotations\\road0.xml')
-    root = tree.getroot()
+    train_data = parse_data('train\\annotations\\')
+    print(train_data)
 
-    for bnd in root.iter('bndbox'):
-        for dir in bnd:
-            print(dir.tag + ": " + dir.text)
 
 if __name__ == '__main__':
     main()
